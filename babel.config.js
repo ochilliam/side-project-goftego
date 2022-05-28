ea93e2cf406f38dc744729cf58e6ec84ccad48ac
Ali Maqsoodi <ochilliam@gmail.com>
2022-05-28 17:20:56 +0430

    chore: add type aliases

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,38 @@
+const tsconfig = require("./tsconfig.json");
+let rawAlias = tsconfig.compilerOptions.paths;
+let alias = {};
+
+for (let x in rawAlias) {
+  alias[x.replace("/*", "")] = rawAlias[x].map((p) => p.replace("/*", ""));
+}
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["tailwindcss-react-native/babel"],
+    plugins: [
+      "tailwindcss-react-native/babel",
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          extensions: [
+            ".ios.js",
+            ".android.js",
+            ".ios.jsx",
+            ".android.jsx",
+            ".js",
+            ".ios.ts",
+            ".android.ts",
+            ".ios.tsx",
+            ".android.tsx",
+            ".ts",
+            ".tsx",
+            ".json",
+          ],
+          alias,
+        },
+      ],
+    ],
   };
 };
